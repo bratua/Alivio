54b2ac0f1ea4a37ebbc6236f2c52d9a625d47d38
Sergey Lisoviy <serlipaua@gmail.com>
2022-12-04 22:44:49 +0200

    mob menu

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,13 +1,15 @@
 (() => {
   // const mobileMenu = document.querySelector('.js-menu-container');
-  const mobileMenu = document.querySelector('[data-menu-button]');
+  const mobileMenu = document.querySelector('[menu-mobile]');
+  const mobileMenuButton = document.querySelector('[data-menu-button]');
   // const openMenuBtn = document.querySelector('.js-open-menu');
   // const closeMenuBtn = document.querySelector('.js-close-menu');
 
   const toggleMenu = () => {
     const isMenuOpen =
-      mobileMenu.getAttribute('aria-expanded') === 'true' || false;
-    mobileMenu.setAttribute('aria-expanded', !isMenuOpen);
+      mobileMenuButton.getAttribute('aria-expanded') === 'true' || false;
+    mobileMenuButton.setAttribute('aria-expanded', !isMenuOpen);
+    mobileMenuButton.classList.toggle('is-open');
     mobileMenu.classList.toggle('is-open');
     // openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
 
@@ -17,7 +19,7 @@
     // bodyScrollLock[scrollLockMethod](document.body);
   };
 
-  mobileMenu.addEventListener('click', toggleMenu);
+  mobileMenuButton.addEventListener('click', toggleMenu);
 
   // openMenuBtn.addEventListener('click', toggleMenu);
   // closeMenuBtn.addEventListener('click', toggleMenu);
