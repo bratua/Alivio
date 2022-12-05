92fdeeeef2813a9e66ec30f44c5d1abc0d867745
bratua <serlipaua@gmail.com>
2022-12-05 07:07:38 +0000

    Deploying to gh-pages from @ bratua/alivio@7ab9da410f9a896dfecabe3d97d111ab142c48e5 🚀

---
+++ index.1b99007e.js
@@ -0,0 +1,2 @@
+(()=>{const e=document.querySelector("[data-mobile]"),t=document.querySelector("[data-menu-button]");t.addEventListener("click",(()=>{const a="true"===t.getAttribute("aria-expanded")||!1;t.setAttribute("aria-expanded",!a),t.classList.toggle("is-open"),e.classList.toggle("is-open")}))})();
+//# sourceMappingURL=index.1b99007e.js.map
