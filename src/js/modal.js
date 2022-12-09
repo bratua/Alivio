6d5054c48c6c61b955c2cef62e884777b101bac6
Sergey Lisoviy <serlipaua@gmail.com>
2022-12-09 12:03:14 +0200

    working at form

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,21 +1,27 @@
 const refs = {
   openModalBtn: document.querySelector('.open-button'),
+  openModalLink: document.querySelector('.open-link'),
   closeModalBtn: document.querySelector('.close-button'),
   modal: document.querySelector('.backdrop'),
   bodyModal: document.querySelector('body'),
 };
 
-console.log(refs.bodyModal);
+// console.log(refs.openModalLink);
 
 refs.closeModalBtn.addEventListener('click', closeModal);
 refs.openModalBtn.addEventListener('click', openModal);
+refs.openModalLink.addEventListener('click', openModal);
 
 function closeModal() {
   refs.modal.classList.add('is-hidden');
   refs.bodyModal.classList.remove('modal-open');
 }
 
-function openModal() {
+function openModal(evt) {
+  evt.preventDefault();
+  // console.log(evt);
+  if (evt.target.classList.contains('open-link')) {
+  }
   refs.modal.classList.remove('is-hidden');
   refs.bodyModal.classList.add('modal-open');
 }
