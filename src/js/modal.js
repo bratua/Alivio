05ab6114eaf730db5ddfcfc267c1cf1e81ed8958
Sergey Lisoviy <serlipaua@gmail.com>
2022-12-09 10:11:33 +0200

    modal in progress

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,17 +1,21 @@
-console.log('MODAL');
-
 const refs = {
   openModalBtn: document.querySelector('.open-button'),
   closeModalBtn: document.querySelector('.close-button'),
   modal: document.querySelector('.backdrop'),
+  bodyModal: document.querySelector('body'),
 };
 
+console.log(refs.bodyModal);
+
 refs.closeModalBtn.addEventListener('click', closeModal);
+refs.openModalBtn.addEventListener('click', openModal);
 
 function closeModal() {
-  console.log('close !!!');
+  refs.modal.classList.add('is-hidden');
+  refs.bodyModal.classList.remove('modal-open');
 }
 
 function openModal() {
-  console.log('open !!!');
+  refs.modal.classList.remove('is-hidden');
+  refs.bodyModal.classList.add('modal-open');
 }
