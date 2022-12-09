c5fb2347935c009d35ceff99510bb6ca2e2f4e39
bratua <serlipaua@gmail.com>
2022-12-09 15:22:15 +0000

    Deploying to gh-pages from @ bratua/alivio@bf85a17af0a64d2aa38f909823f2d20b975a02bf 🚀

--- index.1d89f8ce.js
+++
@@ -1,2 +0,0 @@
-console.log("MODAL");({openModalBtn:document.querySelector(".open-button"),closeModalBtn:document.querySelector(".close-button"),modal:document.querySelector(".backdrop")}).closeModalBtn.addEventListener("click",(function(){console.log("close !!!")}));
-//# sourceMappingURL=index.1d89f8ce.js.map
--- index.3ab39777.js
+++
@@ -1,2 +0,0 @@
-console.log("MODAL"),[document.querySelector(".open-button"),document.querySelector(".close-button"),document.querySelector(".backdrop")][1].addEventListener("click",(function(){console.log("close !!!")}));
-//# sourceMappingURL=index.3ab39777.js.map
